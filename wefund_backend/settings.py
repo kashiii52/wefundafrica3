e3cdbca81405e86b4dbc93ca068771124b727cf5
hassan-zamir-mlsense <hassan.zamir@mlsense.com>
2023-11-16 13:53:30 +0500

    file 9

--- wefund_backend/settings.py
+++ wefund_backend/settings.py
@@ -123,7 +123,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR,'wefund-africa/build')
+            os.path.join(BASE_DIR,'wefund-africa/build'),
             os.path.join(BASE_DIR,'wefund_backend/build')
             ],
         'APP_DIRS': True,
