828ac7902ed3df4a72f458a6940b5468e0060bff
hassan-zamir-mlsense <hassan.zamir@mlsense.com>
2023-11-16 06:34:19 +0500

    pushing files 5

--- wefund_backend/views.py
+++ wefund_backend/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+import os
+from pathlib import Path
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 def index(request):
-    return render(request,'index.html')
+    return render(request,'index.html') or render(request,os.path.join(BASE_DIR,'wefund-africa/build/index.html')) 
