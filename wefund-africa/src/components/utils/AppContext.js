d661766421042dde9703f82cada16c95646c5986
HZ-Innovo <hassan.zamir@innovotechnologies.com>
2023-12-05 11:47:17 +0500

    8000 removed from token ip

--- wefund-africa/src/components/utils/AppContext.js
+++ wefund-africa/src/components/utils/AppContext.js
@@ -16,7 +16,7 @@ export const Provider = ({ children }) => {
   );
 
   const backendRoot =
-    "http://127.0.0.1:8000";
+    "http://127.0.0.1";
   let [user, setUser] = useState(() =>
     localStorage.getItem("authTokens")
       ? jwt_decode(localStorage.getItem("authTokens"))
